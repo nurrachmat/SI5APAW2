b3f26a1555403c9559668d0bd7777e161f099439
Nur Rachmat <rachmat.nur@gmail.com>
2025-09-16 16:57:42 +0700

    res.json dan res.sendfile

--- index.js
+++ index.js
@@ -7,12 +7,45 @@ app.get("/", (req, res) => {
 } )
 
 app.get("/about", (req, res) => {
-    res.send("About Us");
+    res.send("About Us - UMDP");
 } )
 
 app.get("/contact", (req, res) => {
-    res.send("Contact Us");
+    // res.send("Contact Us");
+    res.sendFile(__dirname + "/contact.html");
 })
+
+app.get("/mahasiswa", (req, res) => {
+    res.json({
+        status: "success",
+        message: "Data mahasiswa",
+        data: ["Ritzy", "Naufal", "Marcel"]
+    })
+})
+
+app.get("/nilai", (req, res) => {
+    // silakan dicoba
+    res.json({
+         status: "success",
+        message: "Data nilai",
+        data: [
+            {mk: "Bahasa Indonesia", nilai: "A"},
+            {mk: "PAW 1", nilai: "A"},
+            {mk: "PAB 1", nilai: "B+"}
+        ]
+    })
+})
+
+app.get("/fakultas/:id", (req, res) => {
+    // res.send(`Fakultas id : ${req.params.id}`);
+    res.send("Fakultas ID: " + req.params.id);
+} )
+
+// jika route yang diakses tidak terdaftar, maka ditampilkan Not Found
+app.use("/", (req, res) => {
+    res.status(404); // http response code (not found)
+    res.send("<h1>Not Found</h1>")
+} )
 
 app.listen(port, () => {
     console.log(`Server dapat diakses : http://localhost:${port}`);
